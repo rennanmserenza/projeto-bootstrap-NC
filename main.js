55fa7fb9a98a04349e7cfb8d3f8a851458887222
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2021-12-20 16:52:06 -0300

    [NEW] correção de reload de página ao cadastrar formulário de forma incorreta. Criação de um segundo formulário AGENDE UMA VISITA. Modificação da função de envio de formulário para trabalhar com dois formulários diferentes por parâmetro. Estilização do formulário via Bootstrap e correção de espaçamento.

--- main.js
+++ main.js
@@ -1,40 +1,71 @@
-function envia_email(){
-    var mail, name, job, message;
+function envia_email(i) {
+    var mail, name;
 
-    name = document.getElementById("name-form").value;
-    mail = document.getElementById("mail-form").value;
-    job = document.getElementById("job-form").value;
-    message = document.getElementById("experience").value;
-    
-    if (name != "" && mail != "" && job != "" && message != "" && mail.includes(".com")) {
-        document.getElementById("envio").action = "https://api.staticforms.xyz/submit"
-        alert('Email enviado com sucesso');                  
+    name = document.getElementsByName("name").value;
+    mail = document.getElementByName("email").value;
+
+    if (name == null || name == '') {
+        alert('O campo de nome não foi preenchido.');
     }
-    else {
-        if (name == "" && mail == "" && job == "" && message == "") {
-            alert('Todos os campos devem ser preenchidos.');
-            document.location.reload(false);
+    else if (mail == null || mail == '') {
+        alert('O campo de email não foi preenchido');
+    }
+    else if (!mail.includes(".com")) {
+        alert("Digite um e-mail válido.");
+    }
+
+    if (i == 1) {
+        var job, message;
+        
+        job = document.getElementById("job-form").value;
+        message = document.getElementById("experience").value;
+        
+        if (name != "" && mail != "" && job != "" && message != "" && mail.includes(".com")) {
+            document.getElementById("envio").action = "https://api.staticforms.xyz/submit"
+            alert('Email enviado com sucesso');                  
         }
-        else if (name == null || name == '') {
-            alert('O campo de nome não foi preenchido.');
-            document.location.reload(false);
+        else {
+            if (name == "" && mail == "" && job == "" && message == "") {
+                alert('Todos os campos devem ser preenchidos.');
+            }
+            else if (job == null || job == '') {
+                alert('O campo área de experiencia não foi preenchido.');
+            }
+            else if (message == null || message == '') {
+                alert('O campo sobre sua experiência não foi preenchido.');
+            }
         }
-        else if (mail == null || mail == '') {
-            alert('O campo de email não foi preenchido');
-            document.location.reload(false);
+    }
+    else if (i == 2) {
+        var fone, cpf, cep, logradouro, numero, bairro, cidade, estado;
+        
+        fone = document.getElementById("fone-form").value;
+        cpf = document.getElementById("cpf-form").value;
+
+        cep = document.getElementById("cep").value;
+        logradouro = document.getElementById("endereco").value;
+        numero = document.getElementById("numero-form").value;
+        bairro = document.getElementById("bairro").value;
+        cidade = document.getElementById("cidade").value;
+        estado = document.getElementById("uf").value;
+
+        if (name != "" && mail != "" && job != "" && message != "" && mail.includes(".com")
+         && fone.length() >= 10 && cpf.length() >= 11 && cep.length >= 8 && logradouro != ""
+         && numero != "" && bairro != "" && cidade != "" && estado != "") {
+            document.getElementById("envio").action = "https://api.staticforms.xyz/submit"
+            alert('Email enviado com sucesso');                  
         }
-        else if (!mail.includes(".com")) {
-            alert("Digite um e-mail válido.");
-            document.location.reload(false);
-        }
-        else if (job == null || job == '') {
-            alert('O campo área de experiencia não foi preenchido.');
-            document.location.reload(false);
-        }
-        else if (message == null || message == '') {
-            alert('O campo sobre sua experiência não foi preenchido.');
-            document.location.reload(false);
+        else if (name == "" || mail == "" || job == "" || message == "" || fone == "" 
+        || cpf == "" || cep == "" || logradouro == "" || numero == "" || bairro == "" 
+        || cidade == "" || estado == "") {
+            alert('Todos os campos devem ser preenchidos.');    
         }
     }
 }
 
+mascaraCep = function() {
+    var cep = document.getElementById("cep");
+    
+    if (cep.value.length == 5)
+    cep.value = cep.value + "-"
+}
